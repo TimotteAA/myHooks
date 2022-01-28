5e9f9d3f66d82fcbe6abb9dfed159913ca96fd48
timotte <azurlane202018@gmail.com>
2022-01-28 19:23:15 +0800

    增强useState，可以传入第二个回调参数

---
+++ README.md
@@ -0,0 +1,8 @@
+# 一些自定义 hooks
+
+看到的，自己练习的一些可能有趣的自定义 hook，收集起来以便不时之需。
+
+## 列表
+
+1. 使用 useReducer、useContext 实现极简 redux
+2. 使用 useState、useRef、useEffect 实现的类组件 setState 可以传入更新完 state 后执行的回调
--- useState模仿类组件的setState.js
+++ useState模仿类组件的setState.js
@@ -2,4 +2,24 @@ import { useState, useRef, useEffect } from "react";
 
 function useMyState(initialState) {
   const [state, setState] = useState(initialState);
+
+  let isFunction = useRef();
+  // 增强原来的setState，可以传入第二个回调
+  const mySetState = (state, cb) => {
+    // 执行原来的setState
+    setState((prevState) => {
+      // 记录第二个cb
+      isFunction.current = cb;
+      return typeof state === "function" ? state(prevState) : state;
+    });
+  };
+  useEffect(() => {
+    if (isFunction.current) {
+      isFunction.current();
+    }
+  });
+
+  return [state, mySetState];
 }
+
+export default useMyState;
